71ad7173e431b4e1dae62d3cbd1909c9c04b8434
quan11 <quanpdhe170415@fpt.edu.vn>
2023-10-27 13:52:28 +0700

    create banner
    create banner

---
+++ src/components/Common/Banner.js
@@ -0,0 +1,40 @@
+import {Swiper} from "swiper/react";
+import {Autoplay, Navigation, Pagination} from "swiper/modules";
+import {SwiperSlide} from "swiper/vue";
+
+const Banner = () => {
+    return (
+        <div>
+            <>
+                <Swiper spaceBetween={30}
+                    centeredSlides={true}
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+                        loop={true}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        navigation={true}
+                        modules={[Autoplay, Pagination, Navigation]}
+                        className="banner-swiper"
+                >
+                    <SwiperSlide>
+                        <img className="d-block w-100" src='../../assets/Banner/Banner-web.png'/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img className="d-block w-100" src='../../assets/Banner/1.png'/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img className="d-block w-100" src='../../assets/Banner/2.png'/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img className="d-block w-100" src='../../assets/Banner/3.png'/>
+                    </SwiperSlide>
+                </Swiper>
+            </>
+        </div>
+    );
+}
+export default Banner;
