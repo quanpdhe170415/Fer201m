eea92b6179b5298fc03c6a766dec1ddb5648951e
quan11 <quanpdhe170415@fpt.edu.vn>
2023-10-27 16:31:35 +0700

    update change password
    update change password

--- src/components/Auth/ChangePassword/ChangePassword.js
+++ src/components/Auth/ChangePassword/ChangePassword.js
@@ -1,6 +1,8 @@
-import {useState} from "react";
-
-const ChangePassword = ({userParams, changePasswordMode, handleToggleToasr1}) => {
+import {useEffect, useState} from "react";
+const CryptoJS = require("crypto-js");
+import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
+import ToastComponent from "../../custom/Toast";
+const ChangePassword = ({userParams, changePasswordMode, handleToggleToast}) => {
     const [user, setUser] = useState(userParams);
     const [matching, setMatching] = useState(false);
     const [password, setPassword] = useState();
@@ -8,4 +10,186 @@ const ChangePassword = ({userParams, changePasswordMode, handleToggleToasr1}) =>
     const [showToast, setShowToast] = useState(false);
     const [message, setMessage] = useState("");
     const [users, setUsers] = useState();
+    useEffect(() => {
+        fetch("http://localhost:9999/api/users")
+            .then((response) => response.json())
+            .then((data) => {
+                setUsers(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            })
+    }, []);
+    const handleToggleTOast = () => {
+        setShowToast(!showToast);
+    };
+    const Validate = () => {
+        const REGEX_PASSWORD = /^.{8,}$/;
+        if (!REGEX_PASSWORD.test(password2)) {
+            setMessage("Password must be at least 8 characters long");
+            return false;
+        }
+        return true;
+    };
+    const handleButtonClick = () => {
+        if (matching) {
+            const result = window.confirm(
+                "Are you sure to change your password."
+            );
+            const current_password = document.getElementById("current_password").value;
+            if (result && matching && CryptoJS.MD5(current_password).toString() === user.password) {
+                if (!Validate()) {
+                    handleToggleTOast();
+                    return;
+                }
+                user.password = CryptoJS.MD5(password).toString();
+                const updatedUsers = users.map((item) => {
+                    if (item.id == user.id) {
+                        return user;
+                    }
+                    return item;
+                });
+                fetch("http://localhost:9999/api/users", {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(updatedUsers),
+                })
+                    .then((response) => response.json())
+                    .then((data) => {
+                        console.log(data);
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    });
+                localStorage.setItem("UserID", JSON.stringify(user));
+                changePasswordMode();
+                handleToggleTOast();
+            } else {
+                setMessage("Please try again");
+                handleToggleTOast();
+            }
+        }
+        ;
+        const handleConfirmPasswordChange = () => {
+            if (password2 === password) {
+                document.getElementById("message").style.color = "green";
+                document.getElementById("message").innerHTML = "Matching";
+                setMatching(true);
+            } else {
+                document.getElementById("message").style.color = "red";
+                document.getElementById("message").innerHTML = "Not matching";
+                setMatching(false);
+            }
+        };
+        return (
+            <Container
+                className="bootstrap snippets bootdey border-0"
+                style={{marginTop: "20px"}}
+            >
+                <ToastComponent
+                    message={message}
+                    showToast={showToast}
+                    handleCloseToast={handleToggleToast}
+                />
+
+                <div
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between",
+                    }}
+                >
+                    <h1 className="text-primary">Change Password</h1>
+                    <Button
+                        variant="primary"
+                        onClick={() => {
+                            changePasswordMode();
+                        }}
+                    >
+                        Return
+                    </Button>
+                </div>
+                <hr/>
+                <Row>
+                    <Col md={3}>
+                        <Card.Body>
+                            <img
+                                src="https://enka.network/ui/UI_AvatarIcon_Ganyu.png"
+                                className="avatar img-circle img-thumbnail"
+                                alt="avatar"
+                            />
+                        </Card.Body>
+                    </Col>
+                    <Col md={9} className="personal-info ">
+                        <h3>Change Password</h3>
+                        <Form>
+                            <Form.Group as={Row} className="change-pass-input">
+                                <Form.Label column lg={3} className="control-label">
+                                    Password:
+                                </Form.Label>
+                                <Col lg={8}>
+                                    <Form.Control
+                                        required
+                                        placeholder="Password(*)"
+                                        type="password"
+                                        name="password"
+                                        id="current_password"
+                                    />
+                                </Col>
+                            </Form.Group>
+                            <Form.Group as={Row} className="change-pass-input">
+                                <Form.Label column lg={3} className="control-label">
+                                    New Password:
+                                </Form.Label>
+                                <Col lg={8}>
+                                    <Form.Control
+                                        id="newpass"
+                                        required
+                                        placeholder="Enter New Password(*)"
+                                        type="password"
+                                        name="newpass"
+                                        onKeyUp={handleConfirmPasswordChange}
+                                        onChange={(e) => {
+                                            setPassword2(e.target.value);
+                                        }}
+                                    />
+                                </Col>
+                            </Form.Group>
+                            <Form.Group as={Row} className="change-pass-input">
+                                <Form.Label column lg={3} className="control-label">
+                                    Confirm Password:
+                                </Form.Label>
+                                <Col lg={8}>
+                                    <Form.Control
+                                        id="newpass2"
+                                        onKeyUp={handleConfirmPasswordChange}
+                                        required
+                                        placeholder="Confirm New Password(*)"
+                                        type="password"
+                                        name="newpass2"
+                                        onChange={(e) => {
+                                            setPassword(e.target.value);
+                                        }}
+                                    />
+                                </Col>
+                            </Form.Group>
+                        </Form>
+                    </Col>
+                </Row>
+                <div>
+                    <p style={{marginLeft: "300px"}} id="message"></p>
+                </div>
+                <div style={{marginLeft: "300px", color: "red"}}></div>
+                <div className="form-group" style={{textAlign: "center"}}>
+                    <Button onClick={() => handleButtonClick()} variant="success">
+                        Save
+                    </Button>
+                </div>
+            </Container>
+        );
+    }
 }
+
+export default ChangePassword;
